62be3c4378ddb0d47ac25bb98e729d9d433089a9
FelipeJLopezA <flopez@lab.ztz.ai>
2024-10-21 15:34:41 -0300

    cambios funciona

---
+++ public/map.js
@@ -0,0 +1,8 @@
+maptilersdk.config.apiKey = 'g3kiSTMPNITYCpxUZFry';
+    const map = new maptilersdk.Map({
+      container: 'map',
+      style: 'a59df537-1a82-4fea-9b90-7d6857807ebe',
+      center: [16.62662018, 49.2125578],
+      zoom: 14 
+    });
+
